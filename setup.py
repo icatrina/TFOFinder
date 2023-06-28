1a4f9bce81c24b2c06ce5fbd0e8f6c6b0b34b859
Irina E. Catrina <iecatrina@gmail.com>
2023-06-28 19:19:42 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='TFOFinder',
+      version='1.0',
+      description='Find TFO probes',
+      url='https://github.com/icatrina/TFOFinder',
+      author='Irina E. Catrina',
+      author_email='iecatrina@gmail.com',
+      license='Yeshiva University',
+      #packages=['TFOFinder'],
+      install_requires=[
+          'pandas', 'Biopython<1.76'],
+
+
+      zip_safe=False,
+      platforms=['any'])
